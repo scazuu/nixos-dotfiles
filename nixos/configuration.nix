5efe8282526850fb4bd9a9f9e9c34c8c9168a03f
scazuu <zafferahm@gmail.com>
2026-08-20 21:51:00 +0530

    fix: change vscode to vscode-fhs + the fhs version allows for extensions to work, a wrapper for the base application.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -20,7 +20,6 @@
   # Kernel Settings.
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernel.sysctl."fs.inotify.max_user_watches" = 524288; # to allow jdk's to read files without worrying about limits
-  boot.kernelModules = [ "i2c-dev" ];
 
   # FileSystem Security.
   fileSystems."/boot".options = lib.mkForce [ "umask=0077" ];
@@ -91,7 +90,7 @@
 	google-chrome
 	spotify
 	git
-	vscode
+	vscode-fhs
 	jetbrains.idea
 	jdk21
 	godot
@@ -135,28 +134,8 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
   # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
   # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "26.05"; # Did you read the comment?
 
 }
